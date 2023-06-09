87be62a13c54fcc0daedd87c077e3b9fe4425676
claudia <claudia.iacob@hotmail.com>
2023-06-09 17:28:58 +0200

    Login y password hecho

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,77 @@
+<x-layout>
+
+  <x-slot name="title"> myLinkedup - {{ __('Regístrate') }}</x-slot>
+  <div class="container-fluid">
+      <div class="row justify-content-center">
+          <div class="col-md-8">
+              <div class="card card_login">
+                  <div class="card-header mi_letra">{{ __('Regístrate') }}</div>
+  
+                  <div class="card-body">
+                      <form method="POST" action="{{ route('register') }}">
+                          @csrf
+  
+                          <div class="row mb-3">
+                              <label for="name" class="col-md-4 col-form-label text-md-end mi_letra">{{ __('Nombre') }}</label>
+  
+                              <div class="col-md-6">
+                                  <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+  
+                                  @error('name')
+                                      <span class="invalid-feedback" role="alert">
+                                          <strong>{{__($message)}}</strong>
+                                      </span>
+                                  @enderror
+                              </div>
+                          </div>
+  
+                          <div class="row mb-3">
+                              <label for="email" class="col-md-4 col-form-label text-md-end mi_letra">{{ __('Email') }}</label>
+  
+                              <div class="col-md-6">
+                                  <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+  
+                                  @error('email')
+                                      <span class="invalid-feedback" role="alert">
+                                          <strong>{{__($message)}}</strong>
+                                      </span>
+                                  @enderror
+                              </div>
+                          </div>
+  
+                          <div class="row mb-3">
+                              <label for="password" class="col-md-4 col-form-label text-md-end mi_letra">{{ __('Contraseña') }}</label>
+  
+                              <div class="col-md-6">
+                                  <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+  
+                                  @error('password')
+                                      <span class="invalid-feedback" role="alert">
+                                          <strong>{{__($message)}}</strong>
+                                      </span>
+                                  @enderror
+                              </div>
+                          </div>
+  
+                          <div class="row mb-3">
+                              <label for="password-confirm" class="col-md-4 col-form-label text-md-end mi_letra">{{ __('Confirmar Contraseña') }}</label>
+  
+                              <div class="col-md-6">
+                                  <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                              </div>
+                          </div>
+  
+                          <div class="row mb-0">
+                              <div class="col-md-6 offset-md-4">
+                                  <button type="submit" class="mi_boton mi_letra">
+                                      {{ __('Regístrate') }}
+                                  </button>
+                              </div>
+                          </div>
+                      </form>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  </x-layout>
