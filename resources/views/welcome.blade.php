788814d0bc66e6073bc3edaa48fe474f2fff7d6d
claudia <claudia.iacob@hotmail.com>
2023-06-12 19:55:15 +0200

    Registro hecho

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,55 +1,22 @@
 <x-layout>
-
-  
     <x-slot name='title'>{{__('myLinkedup')}}</x-slot>
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-12">
-                <h1 class="text-center titulo_letra">{{__('Bienvenido a tu comunidad profesional')}}</h1>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
             </div>
         </div>
-        {{--
-         <div class="row">
-            @forelse($ads as $ad)
-            <div class="col-12 col-md-3 d-flex justify-content-center">
-                <a href="{{route("ads.show",$ad)}}" class="card-title text-decoration-none text-dark">
-                <div class="card mi_card" style="width: 43rem;">
-                  @if ($ad->images()->count() > 0)
-                     <img src="{{$ad->images()->first()->getUrl(400,300)}}" class="card-img-top mi_img" alt="...">
-                  @else
-                  <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
-                  @endif
-                </a>
-                    <div class="card-body">
-                        <a href="{{route("ads.show",$ad)}}" class="card-title text-decoration-none text-dark"> {{($ad->title)}}</a>
-                        <h6 class="card-subtitle mb-2 text fs-4">€<a href="{{route("ads.show",$ad)}}" class="card-title text-decoration-none text-dark"> {{$ad->price}}</a></h6>
-                        {{-- <p class="card-text"> {{$ad->body}}</p> --}}
-                       {{-- <div>
-                            <a href="{{route("ads.show",$ad)}}" class="card-title text-decoration-none text-dark">
-                                <p class="bi bi-geo-alt-fill"> {{$ad->location}}</p>
-                            </a>
-                        </div>
-                        <div class="card-subtitle mb-2">
-                            <a href="{{route('category.ads',$ad->category)}}" class="text-decoration-none nav_letra">#{{$ad->category->name}}</a>
-                            <a href="{{route("ads.show",$ad)}}" class="card-title text-decoration-none text-dark">
-                            <i>{{$ad->created_at->format('d/m/Y')}}</i>
-                            </a>
-                        </div> --}}
-                        {{-- <div class="card-subtitle mb-2">
-                            <small> Vendido por: {{ $ad->user->name }}</small>
-                        </div>  ---estaba comentado desde arriba hasta aqui
-                    </div>
-                </div>
-            
-            </div>
-            @empty--}}
-          {{--  <div class="col-12">
-                  <h2>{{__('Uyy.. parece que no hay nada')}}</h2>
-                  <a href="{{route('ads.create')}}" class="btn btn-success me-3">{{__('Vende tu primer objeto')}}</a> {{__('o')}} <a href="{{route('home')}}" class="mi_boton">{{__('Vuelve a la home')}}</a> 
-              </div>
-             @endforelse 
-        </div> --}}
     </div>
-
-
+</div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('home');
 
 // Route::get('/', [PublicController::class, 'index'])->name('home');
 
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
